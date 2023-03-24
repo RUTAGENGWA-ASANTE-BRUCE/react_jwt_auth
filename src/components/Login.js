f7ef7fbe3e3f3b28c2991daebbe468920867f55a
RUTAGENGWA_ASANTE_BRUCE <rutagengwabruce@gmail.com>
2023-03-24 15:04:00 +0200

    [feat] integration with backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import useAuth from '../hooks/useAuth';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 import axios from '../api/axios';
-const LOGIN_URL = '/auth';
+const LOGIN_URL = '/api/login';
 
 const Login = () => {
     const { setAuth } = useAuth();
@@ -32,15 +32,12 @@ const Login = () => {
 
         try {
             const response = await axios.post(LOGIN_URL,
-                JSON.stringify({ user, pwd }),
+                JSON.stringify({ "username":user,"password": pwd }),
                 {
                     headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
                 }
             );
-            console.log(JSON.stringify(response?.data));
-            //console.log(JSON.stringify(response));
-            const accessToken = response?.data?.accessToken;
+            const accessToken = response?.data?.token;
             const roles = response?.data?.roles;
             setAuth({ user, pwd, roles, accessToken });
             setUser('');
